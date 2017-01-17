05ce3ecfd3e2cba55e7fd36516690bd4821fc9e4
SergoRz <sanchez13rz@gmail.com>
2017-01-17 21:04:17 +0100

    Clase dbhelper

--- app/src/main/java/com/example/versus/birthdayhelper/MainActivity.java
+++ app/src/main/java/com/example/versus/birthdayhelper/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.versus.birthdayhelper;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
         ContactosDbHelper usdbh = new ContactosDbHelper(this);
 
         SQLiteDatabase db = usdbh.getWritableDatabase();
+
+        if(db != null){
+            Log.d("BD","Se ha abierto correctamente");
+        } else{
+            Log.d("BD","No se ha abierto correctamente");
+        }
     }
 }
