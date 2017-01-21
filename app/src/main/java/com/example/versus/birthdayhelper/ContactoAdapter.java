b9785184d61ed140967701082823d10d444dbe68
ecabab <ecabab@gmail.com>
2017-01-21 13:46:33 +0100

    Cambiuo

--- app/src/main/java/com/example/versus/birthdayhelper/ContactoAdapter.java
+++ app/src/main/java/com/example/versus/birthdayhelper/ContactoAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -21,7 +22,6 @@ public class ContactoAdapter extends ArrayAdapter<Contacto> {
      * Constructor de la clase
      * @param context Contexto de la Activity
      * @param array_contactos ArrayList de los contactos
-     * @param array_contactos
      */
     public ContactoAdapter(Context context, ArrayList<Contacto> array_contactos) {
         super(context, -1, array_contactos);
@@ -39,16 +39,20 @@ public class ContactoAdapter extends ArrayAdapter<Contacto> {
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = (LayoutInflater) contexto.getSystemService(Context.LAYOUT_INFLATER_SERVICE); //Se crea el LayoutInflater
 
-        View layoutTitular = inflater.inflate(R.layout.fila, parent, false); //Instancia el layout personalizado(fila) en esta Vista.
+        View layoutContacto = inflater.inflate(R.layout.tuplacontacto, parent, false); //Instancia el layout personalizado(fila) en esta Vista.
 
-        TextView tituloTextView = (TextView) layoutTitular.findViewById(R.id.textViewTitulo); //TextView del titulo, se asocia con el del XML
-        TextView subtitulo = (TextView) layoutTitular.findViewById(R.id.textViewSubtitulo); //TextView del subtitulo, se asocia con el del XML
+        ImageView ivFoto = (ImageView) layoutContacto.findViewById(R.id.imageViewFoto );
+        TextView tvNombre = (TextView) layoutContacto.findViewById(R.id.textViewNombre); //TextView del titulo, se asocia con el del XML
+        TextView tvTelefono = (TextView) layoutContacto.findViewById(R.id.textViewNumero); //TextView del subtitulo, se asocia con el del XML
+        TextView tvAviso = (TextView) layoutContacto.findViewById(R.id.textViewAviso);
 
-        Titular titularActual = array_titulares.get(position); //Se recorren los titulares
+        Contacto contactoActual = array_contactos.get(position); //Se recorren los titulares
 
-        tituloTextView.setText(titularActual.getTitulo()); //Se recoge el titulo del titular en el que se encuentra
-        subtitulo.setText(titularActual.getSubtitulo()); //Se recoge el subtitulo del titular en el que se encuentra
+        ivFoto.setImageDrawable(R.id.); //Se recoge el titulo del titular en el que se encuentra
+        tvNombre.setText(contactoActual.getNombre()); //Se recoge el titulo del titular en el que se encuentra
+        tvTelefono.setText(String.valueOf(contactoActual.getTelefono())); //Se recoge el subtitulo del titular en el que se encuentra
 
-        return layoutTitular;
+
+        return layoutContacto;
     }
 }
