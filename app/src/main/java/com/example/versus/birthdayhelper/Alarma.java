b1698343e4c646a61d1db359d16e89af299b4486
SergoRz <sanchez13rz@gmail.com>
2017-01-24 16:37:24 +0100

    SI JODEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEER!!!!!

--- app/src/main/java/com/example/versus/birthdayhelper/Alarma.java
+++ app/src/main/java/com/example/versus/birthdayhelper/Alarma.java
@@ -5,7 +5,6 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-
 import java.util.Calendar;
 
 public class Alarma extends Activity{
