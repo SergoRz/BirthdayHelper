53496b1fbe947d79ffb2331ae603f184d1bc26ba
SergoRz <sanchez13rz@Gmail.com>
2017-01-30 20:26:41 +0100

    Update Alarma.java

--- app/src/main/java/com/example/versus/birthdayhelper/Alarma.java
+++ app/src/main/java/com/example/versus/birthdayhelper/Alarma.java
@@ -1,21 +1,20 @@
 package com.example.versus.birthdayhelper;
 
+import android.annotation.TargetApi;
 import android.app.AlarmManager;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
-import android.support.v4.content.WakefulBroadcastReceiver;
 import android.telephony.SmsManager;
 import android.util.Log;
-
+import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 
 /**
  *
@@ -25,6 +24,8 @@ public class Alarma extends BroadcastReceiver {
     private AlarmManager alarmMgr;
     private PendingIntent alarmIntent;
     private Context mContext;
+    private ContactosDbHelper cdbh = new ContactosDbHelper(mContext); //Clase que se encarga de la base de datos
+    private SQLiteDatabase db; //Base de datos de los contactos
 
     public Alarma(Context mContext){
         this.mContext = mContext;
@@ -32,7 +33,9 @@ public class Alarma extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        sendNotification();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            comprobarNotificaciones();
+        }
     }
 
     public void setAlarma(int hora, int min) {
@@ -50,29 +53,47 @@ public class Alarma extends BroadcastReceiver {
         alarmMgr.setRepeating(AlarmManager.RTC, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, alarmIntent);
     }
 
-    // ENVIA UNA NOTIFICACION DE PRUEBA
-    private void sendNotification() {
-        Contacto oContactoE = new Contacto(10, "Emilio", "628232938", "25/01/2017", 'n', "Hola Emilio, Feliz Cumpleaños!");
-        Contacto oContactoS = new Contacto(10, "Sergio", "665463077", "25/01/2017", 'n', "Hola Sergio, Feliz Cumpleaños!");
+    private void comprobarNotificaciones() {
 
-        Date horaActual = new Date();
-        String hora=horaActual.getHours()+ " " +horaActual.getMinutes();
+        ArrayList<Contacto> contactos;
+        ArrayList<Contacto> cumplen = new ArrayList<>();
 
+        Calendar fecha = Calendar.getInstance();
+        String mes = String.valueOf(fecha.get(Calendar.MONTH));
+        String dia = String.valueOf(fecha.get(Calendar.DAY_OF_MONTH));
+
+        db = cdbh.getWritableDatabase(); //Se obtiene la base de datos de la clase ContactosDbHelper
+        contactos = ContactosDbHelper.cargarContactos(db);
+
+        for(Contacto contacto : contactos){
+            String fechaContacto = contacto.getFechaNacimiento();
+            String tokens[] = fechaContacto.split("/");
+
+            if(tokens[0].equals(dia) && tokens[1].equals(mes)) cumplen.add(contacto);
+        }
+
+        if(cumplen.size() > 0){
+            sendNotificacion();
+
+            for(Contacto contacto : cumplen){
+                sendSMS(contacto);
+            }
+        }
+    }
+
+    public void sendSMS(Contacto oContacto) {
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(oContacto.getTelefono(),null,oContacto.getMensaje(),null,null);
+    }
+
+    public void sendNotificacion(){
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(mContext);
         mBuilder.setSmallIcon(android.R.drawable.btn_star);
         mBuilder.setLargeIcon((((BitmapDrawable) mContext.getResources().getDrawable(R.drawable.notif)).getBitmap()));
         mBuilder.setContentTitle("BirthDay Helper");
-        mBuilder.setContentText(hora);
+        mBuilder.setContentText("Hoy es el cumpleaños de alguien.");
 
         NotificationManager notif = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
         notif.notify(100, mBuilder.build());
-
-        sendSMS(oContactoE);
-    }
-
-    public void sendSMS(Contacto oContacto) {
-        Log.d("SMS","enviado");
-        SmsManager sms = SmsManager.getDefault();
-        sms.sendTextMessage(oContacto.getTelefono(),null,oContacto.getMensaje(),null,null);
     }
 }
